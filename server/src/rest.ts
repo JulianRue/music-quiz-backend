503d5faa6ac66a831b4d7fc23b432a759ff79223
Julian Rütter <x.tronny@hotmail.com>
2020-06-19 21:57:00 +0200

    immer access denied, evtl fehler bei app.use(session({     secret: 'mySecret',

--- server/src/rest.ts
+++ server/src/rest.ts
@@ -4,8 +4,22 @@ import db from "./queries";
 
 export const app = express();
 
+const Keycloak =  require("keycloak-connect");
+var session = require('express-session');
+var memoryStore =  new  session.MemoryStore();
+var keycloak =  new  Keycloak({ store: memoryStore });
+app.use(session({
+    secret: 'mySecret',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+}));
+app.use(keycloak.middleware());
+
+/*
 const keycloak = require('../config/keycloakConfig.js').initKeycloak();
 app.use(keycloak.middleware());
+*/
 
 export const port :number  = 3000;
 console.log("Started rest");
@@ -21,7 +35,7 @@ app.get('/api/', (request, response) => {
     response.json({ info: 'Node.js, Express, REST, and Postgres API' })
 });
 
-app.get('/api/playlist/:count', /*keycloak.protect(['user','admin']),*/ db.getPlaylists);
+app.get('/api/playlist/:count', keycloak.protect('admin'), db.getPlaylists);
 app.get('/api/playlist/id/:id', db.getPlaylistSongsById);
 app.get('/api/playlist/user/:name', db.getPlaylistSongsByName);
 
@@ -32,5 +46,7 @@ app.listen(port, () => {
 function run(){
 
 }
+
+
 
 export default {}
